c580e2fc649600eb7338ec51d85ddf070eb9d5a9
neerajadsul <neeraj.adsul@gmail.com>
2024-03-24 17:40:29 +0000

    Tiny progress towards blinking LED.

--- stm32l0x3/src/main.rs
+++ stm32l0x3/src/main.rs
@@ -1,14 +1,19 @@
 #![no_std]
 #![no_main]
 
-// extern crate panic_halt as _;
+use panic_halt as _;
 
 // mod interrupts;
 
 use cortex_m_rt::entry;
+use stm32l0::stm32l0x3::Peripherals;
 
 #[entry]
 fn main() -> ! {
+    let mut ph = Peripherals::take().unwrap();
+
+    ph.GPIOA.odr.write(|port| port.bits(5));
+
     loop {
         
     }
